6d85805915253f9c1fd3259a6a1eec16606d7764
Duong Tung Anh <tunnaduong@gmail.com>
2024-08-18 17:05:19 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  // Change image every 2 seconds
+}
+
+setInterval(() => plusSlides(1), 3000);
